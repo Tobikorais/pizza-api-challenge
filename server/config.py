31ca9fad1cbb76f0420a91d221f4b5e6c3ce00de
Kelvina <Kelvina-web.com>
2025-06-17 19:50:29 +0300

    Adding files

--- server/config.py
+++ server/config.py
@@ -3,5 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///restaurants.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://pizza_user:your_password@localhost/pizzadb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
